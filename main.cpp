4680a0f9cd63073351100a31ef73dbaeec1b7fa5
salnikov <salnikov@dd3b8c80-2a67-46f5-988b-0386dba3f684>
2009-07-02 15:16:05 +0000

    Nothing interesting
    git-svn-id: https://svn.code.sf.net/p/frigate/svn/trunk/frigate@4 dd3b8c80-2a67-46f5-988b-0386dba3f684

--- main.cpp
+++ main.cpp
@@ -13,8 +13,31 @@ void yyerror(const char *err_msg)
 int main(int argc,char **argv)
 {
 
+	FILE *f=NULL;
+	FILE *buf;
+	int flag;
+	if(argc<2)
+	{
+		printf("Incorrect arguments\n");
+		return -1; 
+	}
+
+	f=fopen(argv[1],"r");
+	if(f==NULL)
+	{
+		printf("Can't open file with name '%s'\n",argv[1]);
+		return 1;
+	}
+
 	yydebug=1;
-	yyparse();
+
+	buf=stdin;
+	stdin=f;
+	flag=yyparse();
+	fclose(f);
+	stdin=buf;
+
+	printf("Parsing finished with value %d\n",flag);
 
 	return 0;
 }

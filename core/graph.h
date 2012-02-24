1fb52754e705e6d7cf52caf53d64d27fef4b3d94
kislas <kislas@dd3b8c80-2a67-46f5-988b-0386dba3f684>
2012-02-24 02:12:24 +0000

    git-svn-id: https://svn.code.sf.net/p/frigate/svn/trunk/frigate@27 dd3b8c80-2a67-46f5-988b-0386dba3f684

--- core/graph.h
+++ core/graph.h
@@ -9,37 +9,13 @@
 
 namespace frigate
 {
-
-    class Graph
-    {
-	protected:
-		float version;
-		
-		Code_block header;
-		Code_block root;
-		Code_block tail;
-
-        Char_names char_names;
-
-       
-
-        /**
-         * The main subgraph name in the graph.
-         */
-		char* main_subgraph;
-        
-		Line_dynamic_array<Subgraph>        subgraphs;
-        
-		Line_dynamic_array<External_edge>   external_edges;
-        
-		Line_dynamic_array<Vertex_template> vertex_templates;
-        
-		Line_dynamic_array<Edge_template>   edge_templates;
-    };
+//////////////////////////////
+// Vertices
 
     /**
      * This class contains links to blocks of code in C/C++ for vertices or "header", 
      * "root" and "tail" in graph.
+     * Also it contains info about i/o and calculation costs.
      */
     class Code_block
     {
@@ -57,21 +33,110 @@ namespace frigate
          * then this code block is unusable.
          */ 
         char* file_name;
+
+        int io_volume;
+        int code_volume;
     };
 
-    class Subgraph
+    /**
+     * This class is need for possibility of multiple exchanges in one vertex
+     * It contains IDs of opposite vertices
+     */
+    class Exchange_block
     {
     protected:
-        frigate_name_id_type name_id;
-        char* condition_code;
-        int   prefefined_condition_value;
-        Line_dynamic_array<Vertex> vertices;
-        Line_dynamic_array<Internal_edge> internal_edges;
-        Line_dynamic_array<Control_edge>  control_edges;
-
+    	frigate_name_id_type name_id;
+    	Line_dynamic_array<frigate_name_type_id> send;
+    	Line_dynamic_array<frigate_name_type_id> recv;
     };
 
-    class External_edge
+    /**
+     * Template class contains code blocks and exchange blocks
+     * in arbitrary order. Every vertex must have its template
+     */
+    class Vertex_template
+    {
+    protected:
+    	frigate_name_id_type template_name_id;
+    	Line_dynamic_array<Code_block> code_blocks;		//Need to think about
+    	Line_dynamic_array<Exchange_block> exchanges;	//alternating them
+    };
+
+    /**
+     * Vertex class is specific vertex.
+     * Several vertices may have same template
+     */
+    class Vertex: public Vertex_template
+    {
+    protected:
+    	frigate_name_id_type name_id;
+    };
+
+
+////////////////////////////////////////////
+// Edges
+
+    /**
+     * Class with data for MPI-calls in edges
+     */
+    class Fragment
+    {
+    protected:
+    	char* variable;
+    	char* type;
+    	char* left_border;
+    	char* right_border;
+    };
+
+    /*
+     * Template of edge, contains information about sending and
+     * receiving variables
+     */
+    class Edge_template
+    {
+    protected:
+    	frigate_name_id_type template_name_id;
+
+    	Line_dynamic_array<Fragment> send_fragments;
+    	Line_dynamic_array<Fragment> recv_fragments;
+    };
+
+    /*
+     * Internal edge for subgraph contains sending and receiving vertices coords
+     */
+    class Internal_edge: public Edge_template
+    {
+    	frigate_name_id_type name_id;
+    	frigate_name_id_type template_name_id;
+
+    	/*
+    	 * Send side
+    	 */
+    	frigate_name_id_type send_vertex_id;
+    	frigate_name_id_type send_vertex_exchange_id;
+
+    	/*
+    	 * Recv side
+    	 */
+    	frigate_name_id_type recv_vertex_id;
+    	frigate_name_id_type recv_vertex_exchange_id;
+    };
+
+    /*
+     * Control edge always connect with control vertex
+     */
+    class Control_edge: public Edge_template
+    {
+    	frigate_name_id_type name_id;
+    	frigate_name_id_type template_name_id;
+    	frigate_name_id_type send_vertex_id;
+    	frigate_name_id_type send_vertex_exchange_id;
+    };
+
+    /*
+     * External edge connect vertices in different subgraphs
+     */
+    class External_edge: public Edge_template
     {
     protected:
         frigate_name_id_type name_id;
@@ -94,6 +159,56 @@ namespace frigate
         frigate_name_id_type recv_exchange_block_id;
     };
 
+////////////////////////////////
+// Subgraphs and graph
+
+    /*
+     * Subgraph class contains vertices, internal and control edges.
+     * Condition_code - some condition to start subgraph calculation
+     */
+    class Subgraph
+    {
+    protected:
+        frigate_name_id_type name_id;
+        char* condition_code;
+        int   predefined_condition_value;
+        Line_dynamic_array<Vertex> vertices;
+        Line_dynamic_array<Internal_edge> internal_edges;
+        Line_dynamic_array<Control_edge>  control_edges;
+
+    };
+
+    /*
+     * Graph class contains code blocks for
+     * header - some necessary includes
+     * root - code for execution before any other code (initialization etc.)
+     * tail - code for execution after any other code (cleaning etc.)
+     * subgraphs, external edges, vertex templates and edges templates
+     */
+    class Graph
+    {
+	protected:
+		float version;
+
+		Code_block header;
+		Code_block root;
+		Code_block tail;
+
+        Char_names char_names;
+
+        /**
+         * The main subgraph name in the graph.
+         */
+		char* main_subgraph;
+
+		Line_dynamic_array<Subgraph>        subgraphs;
+
+		Line_dynamic_array<External_edge>   external_edges;
+
+		Line_dynamic_array<Vertex_template> vertex_templates;
+
+		Line_dynamic_array<Edge_template>   edge_templates;
+    };
 
 }
 
